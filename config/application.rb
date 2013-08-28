fa6c459c7230a927d7cc6da3beec225c0b6ab14f
hippoakuna <hippoakuna@gmail.com>
2013-08-28 15:24:15 -0500

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module HippoOne
     # config.i18n.default_locale = :de
     #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js *.scss *.ttf *.otf *.svg *.eot *.woff *.m4a)
     config.assets.precompile = ['*.js', '*.css', '*.png', '*.jpg', '*.jpeg', '*.gif', '*.ttf', '*.otf', '*.svg', '*.eot', '*.woff', '*.m4a']
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
