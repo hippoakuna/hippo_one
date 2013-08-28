d4e3e68782849abc581eb08077407540915c7d07
hippoakuna <hippoakuna@gmail.com>
2013-08-28 00:35:20 -0500

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ HippoOne::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( wookmark/wookmark.js, wookmark/wookmark-audio.js )
+  config.assets.precompile += %w( wookmark/wookmark, wookmark/wookmark-audio )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
