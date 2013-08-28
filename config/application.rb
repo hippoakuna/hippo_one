b6dc135313e30824cb49ddbfb0972a524650470a
hippoakuna <hippoakuna@gmail.com>
2013-08-28 00:09:56 -0500

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module HippoOne
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js *.scss *.ttf *.otf *.svg *.eot *.woff *.m4a)
+    #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js *.scss *.ttf *.otf *.svg *.eot *.woff *.m4a)
+    config.assets.precompile = ['*.js', '*.css', '*.png', '*.jpg', '*.jpeg', '*.gif', '*.ttf', '*.otf', '*.svg', '*.eot', '*.woff', '*.m4a']
   end
 end
